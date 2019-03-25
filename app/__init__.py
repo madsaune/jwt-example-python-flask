597e8fec43cb9e05de2d01cf92dbaadb14be5a7e
Mads Moi-Aune <madsaune@gmail.com>
2019-03-25 20:24:08 +0100

    login route is now working

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 import bcrypt
+import jwt
+import datetime
 
 app = Flask(__name__)
 client = MongoClient('localhost', port=27017)
@@ -15,15 +17,27 @@ def login():
         return jsonify({
             "auth": False,
             "message": "Username or password is incorrect."
-        })
+        }), 401
     
-    if bcrypt.checkpw(r["password"].encode('utf-8'), user['password'].encode('utf-8')):
-        return jsonify({
-            "auth": True,
-            "message": "Username or password was correct."
-        })
-    else:
+    if not bcrypt.checkpw(r["password"].encode('utf-8'), user['password'].encode('utf-8')):
         return jsonify({
             "auth": False,
             "message": "Username or password is incorrect."
-        })
+        }), 401
+    else:
+        payload = {
+            'username': user['username'],
+            'email': user['email'],
+            'name': user['name'],
+            'roles': user['roles'],
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=12)
+        }
+
+        token = jwt.encode(payload, 'helloyellow').decode('utf-8')
+        print(token)
+
+        return jsonify({
+            "auth": True,
+            "token": token,
+            "message": "Username or password was correct."
+        }), 200
